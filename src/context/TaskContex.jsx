913cff972a6ed7b7a1f7a14f16bcb5f3b30b5ad1
Ganesh Devadiga <ganeshmurdeshwar@gmail.com>
2025-09-14 14:24:58 +0530

    new files

---
+++ src/context/TaskContex.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TaskContex = ({children}) => {
+  return (
+    <div>{children}</div>
+  )
+}
+
+export default TaskContex
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import AuthContext from "./context/AuthContext.jsx";
+import TaskContex from "./context/TaskContex.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <AuthContext>
+      <TaskContex>
+        <App />
+      </TaskContex>
+    </AuthContext>
+  </StrictMode>
+);
